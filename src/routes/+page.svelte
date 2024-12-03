a9879e68eca9375a11f13cd2de46dd63ebde37c0
undefined1raven <zlat.dominic.francisc@gmail.com>
2024-12-03 23:13:37 +0200

    mobile landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,12 @@
 	import { globalStyle } from '../stores/globalStyle.svelte';
 	import LandingPage from './components/landingPage/landingPage.svelte';
 	import Logo from './deco/Logo.svelte';
+	import isMobile from '../utils/isMobile';
+	import LandingPageMobile from './components/landingPageMobile/LandingPageMobile.svelte';
 </script>
 
-<LandingPage></LandingPage>
+{#if isMobile() === false}
+	<LandingPage></LandingPage>
+{:else}
+	<LandingPageMobile></LandingPageMobile>
+{/if}
--- src/routes/components/common/ThemeController.svelte
+++ src/routes/components/common/ThemeController.svelte
@@ -1,30 +1,38 @@
 <script>
 	import { globalStyle } from '../../../stores/globalStyle.svelte';
 	import { theme } from '../../../stores/theme.svelte';
+	import isMobile from '../../../utils/isMobile';
 	import LunaDeco from '../../deco/LunaDeco.svelte';
 	import SunDeco from '../../deco/SunDeco.svelte';
 
-	const buttonStyle =
-		'flex hover:bg-color20 transition-all linear duration-75 h-full w-1/2 items-center justify-center';
+	const buttonStyle = `flex hover:bg-color20 transition-all linear duration-75 h-full ${isMobile() ? 'w-full' : 'w-1/2'}  items-center justify-center`;
 </script>
 
-<div class="absolute left-1 top-1 flex h-12 w-32 flex-row rounded-xl border border-color">
-	<button
-		onclick={() => {
-			sessionStorage.setItem('theme', 'light');
-			theme.set('light');
-		}}
-		class={buttonStyle}
-	>
-		<SunDeco color={$globalStyle.color}></SunDeco>
-	</button>
-	<button
-		onclick={() => {
-			sessionStorage.setItem('theme', 'dark');
-			theme.set('dark');
-		}}
-		class={buttonStyle + ' border-l border-color'}
-	>
-		<LunaDeco color={$globalStyle.color}></LunaDeco>
-	</button>
+<div
+	class="absolute left-1 top-1 flex {isMobile() ? 'h-8' : 'h-12'} {isMobile()
+		? 'w-20'
+		: 'w-32'} flex-row rounded-xl border border-color"
+>
+	{#if isMobile() === false || $theme === 'dark'}
+		<button
+			onclick={() => {
+				sessionStorage.setItem('theme', 'light');
+				theme.set('light');
+			}}
+			class={buttonStyle}
+		>
+			<SunDeco color={$globalStyle.color}></SunDeco>
+		</button>
+	{/if}
+	{#if isMobile() === false || $theme === 'light'}
+		<button
+			onclick={() => {
+				sessionStorage.setItem('theme', 'dark');
+				theme.set('dark');
+			}}
+			class={buttonStyle + ' border-l border-color'}
+		>
+			<LunaDeco color={$globalStyle.color}></LunaDeco>
+		</button>
+	{/if}
 </div>
